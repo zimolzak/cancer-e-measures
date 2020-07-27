b92cb821fcd98dca17e26b9a78823bc41cc7948d
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-07-27 17:23:38 -0500

    Link ICD codes with registry and count overlap.

--- ajz-onc-crude-moore.sql
+++ ajz-onc-crude-moore.sql
@@ -1,3 +1,5 @@
+-- LUNG
+
 SELECT TOP (200) 
 [patientsid]
 ,[Sta3n]
@@ -96,9 +98,6 @@ SELECT TOP (200)
   where (SitegpX like 'lung%' or ICDOSite like 'lung%' or PrimarysiteX like 'lung%')
 	and not HistologyIcdo3X like 'small%' and not HistologyIcdo3X  like 'carcinoid%' and not HistologyIcdo3X  like 'neuro%'
 
-
-
-
 -- COUNTS
 
 select count(*)
@@ -107,7 +106,6 @@ select count(*)
 -- 71583
 	and not HistologyIcdo3X like 'small%' and not HistologyIcdo3X  like 'carcinoid%' and not HistologyIcdo3X  like 'neuro%'
 	--62466
-
 
 -- BY CLINICAL
 
@@ -173,7 +171,6 @@ n	Stagegroupingajcc
 407	NA
 221	0
 
-
 I and II = 28,983
 0 I II  = 29204
 III and IV = 27,905
@@ -186,6 +183,7 @@ Null unk na = 14474
 */
 
 -- BY AJCC EXCLUDE SMALL CELL
+-- GOOD STUFF NEXT STATEMENT
 
 SELECT count(*) as n, Stagegroupingajcc 
 FROM [ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5]
@@ -225,6 +223,7 @@ FROM [ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5]
 		and not HistologyIcdo3X like 'small%' and not HistologyIcdo3X  like 'carcinoid%' and not HistologyIcdo3X  like 'neuro%'	
 		and (Stagegroupingajcc = 'III' or Stagegroupingajcc = 'IV')
 --22873
+
 
 
 
@@ -278,8 +277,6 @@ order by n desc
 28	ADENOMA, NOS
 */
 
-
-
 -- bad ones:
 
 /*
@@ -297,7 +294,6 @@ order by n desc
 28	ADENOMA, NOS
 */
 
-
 --false pos examples
 SELECT TOP (200) 
 [patientsid],[Sta3n],[Stagegroupclinical] ,[StageGroupingajcc] ,[SitegpX],[ICDOSite],[PrimarysiteX],[Histologyicdo3X] -- last good one?
@@ -305,7 +301,6 @@ SELECT TOP (200)
   FROM [ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5]
   where (SitegpX like 'colo%' or ICDOSite like 'colo%' or PrimarysiteX like 'colo%')
   and Histologyicdo3X like 'carcinoid%'
-
 
 SELECT count(*)
   FROM [ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5]
@@ -345,11 +340,6 @@ select count(*) as n, [Histologyicdo3X]
 group by Histologyicdo3X
 order by n desc
 -- 104 rows
-/*
-
-*/
-
-
 
 -- double percent
 select count(*) as n, [Histologyicdo3X]
@@ -368,14 +358,7 @@ order by n desc
 
 
 
-
-
-
-
-
-
 /* DONE REFINING WHERE CLAUSE */
-
 
 select TOP (200) 
 [patientsid],[Sta3n],[Stagegroupclinical] ,[StageGroupingajcc] ,[SitegpX],[ICDOSite],[PrimarysiteX],[Histologyicdo3X] -- last good one?
@@ -388,6 +371,8 @@ select TOP (200)
   Histologyicdo3X like '%lymph%' or 
   Histologyicdo3X like 'gastrointestinal strom%' or
   Histologyicdo3X like '%sarcoma%')
+
+-- GOOD STUFF NEXT STATEMENT
 
 select count(*) as n, [StageGroupingajcc] 
   FROM [ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5]
@@ -413,3 +398,146 @@ n	StageGroupingajcc
 28	NA
 */
 
+
+
+
+
+
+
+
+
+/************************ LOOK AT ICD CODES ************************/
+
+-- YOU CAN SKIP THIS INITIAL EXPLORATION AND GO DO WHERE IT SAYS "--RUN"
+
+-- Expect C18 colon, C19 rectosig, C20 rectum
+select top 10 
+icd10sid, icdien, sta3n, ICD10Code
+from CDWWork.dim.ICD10
+where ICD10Code like 'c18%'
+
+select count(*)
+from CDWWork.dim.ICD10
+where ICD10Code like 'c18%' --1300
+or icd10code like 'c19%' or ICD10Code like 'c20%' --1560 for all 3 codes
+
+--lung
+-- C34.0 main bronch, .1 upper, .2 middle, 3 lower, 8 other, 90 unspec
+-- really can just do C34*.
+select top 100
+icd10sid, icdien, sta3n, ICD10Code
+from CDWWork.dim.ICD10
+where ICD10Code like 'c34%'
+
+select count(*)
+from CDWWork.dim.ICD10
+where ICD10Code like 'c34%' --2080
+and Sta3n = 580 -- 16
+
+select * from CDWWork.dim.ICD10 where ICD10Code like 'c34%' and Sta3n = 580
+
+-- temp tables
+
+drop table #ajz_lungca_outpats
+
+select top 100
+patientsid, VisitDateTime, PrimarySecondary, ICD10Code
+into #ajz_lungca_outpats
+from ORD_Singh_202001030D.src.Outpat_VDiagnosis as outpa
+inner join
+cdwwork.dim.icd10 as icd
+on icd.icd10sid = outpa.icd10sid
+where outpa.sta3n=580 and outpa.ICD10SID > 0 and icd.ICD10Code like 'c34%'
+
+select count(*) as N into #ajz_deleteme
+from ORD_Singh_202001030D.src.Outpat_VDiagnosis as outpa
+inner join
+cdwwork.dim.icd10 as icd
+on icd.icd10sid = outpa.icd10sid
+where outpa.sta3n=580 and outpa.ICD10SID > 0 and icd.ICD10Code like 'c34%'
+
+-- 26996 diagnoses in HOU for lung CA, in unk. no. of pts.
+
+--tempdb.dbp.#blah
+
+select * from #ajz_deleteme
+select * from #ajz_lungca_outpats
+select distinct patientsid from #ajz_lungca_outpats -- SELECT TOP 10 visits but 8 PatientSID. Or 100:50.
+
+-- join with registry
+
+select distinct outpa.patientsid, outpa.PrimarySecondary, outpa.ICD10Code
+	,reg.[Stagegroupclinical] ,reg.[StageGroupingajcc] ,reg.[SitegpX],reg.[ICDOSite],reg.[PrimarysiteX],reg.[Histologyicdo3X] 
+      ,reg.[DateDX],reg.[Histologyicdo2X],reg.[SeerSummaryStage2000]
+	  --drop visitDateTime
+from #ajz_lungca_outpats as outpa
+left join
+[ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5] as reg
+on outpa.PatientSID = reg.PatientSID
+order by patientsid
+
+--narrow down more.
+select distinct outpa.patientsid, outpa.ICD10Code
+	,reg.datedx
+	,reg.[Stagegroupclinical] ,reg.[StageGroupingajcc] ,reg.[SitegpX],reg.[ICDOSite],reg.[PrimarysiteX],reg.[Histologyicdo3X] 
+      ,reg.[SeerSummaryStage2000]
+from #ajz_lungca_outpats as outpa
+left join
+[ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5] as reg
+on outpa.PatientSID = reg.PatientSID
+order by patientsid, DateDX
+--insight: people may be in there with more than 1 datedx.
+
+--narrower. 
+select distinct outpa.patientsid 
+	,reg.[SitegpX],reg.[ICDOSite] --reg.[PrimarysiteX] --reg.[Histologyicdo3X] is too granular. So is reg.[SeerSummaryStage2000]
+	, (case when reg.SitegpX like 'lung%' then 'Y' else NULL end) as is_lung
+from #ajz_lungca_outpats as outpa
+left join
+[ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5] as reg
+on outpa.PatientSID = reg.PatientSID
+order by patientsid
+--If include histologyICDO3X:  14 rows instead of 19
+-- Drop histology and seersummary: 12 rows
+-- drop primarySiteX: 11 rows
+
+/* That was nice. Now increase the number in the temp table. */
+
+-- 100 outpat dx --> 65 rows. (?)
+
+
+
+-- GOOD STUFF NEXT STATEMENTS
+--RUN
+drop table #ajz_lungca_sidonly
+
+select distinct top 10000 --10000 = 5 sec = 1544 patientSID
+patientsid
+into #ajz_lungca_sidonly
+from ORD_Singh_202001030D.src.Outpat_VDiagnosis as outpa
+inner join
+cdwwork.dim.icd10 as icd
+on icd.icd10sid = outpa.icd10sid
+where outpa.sta3n=580 and outpa.ICD10SID > 0 and icd.ICD10Code like 'c34%'
+
+--RUN
+drop table #ajz_icd_reg
+
+select distinct lung.patientsid ,reg.[SitegpX],reg.[ICDOSite] 
+into #ajz_icd_reg
+from #ajz_lungca_sidonly as lung
+left join
+[ORD_Singh_202001030D].[Src].[Oncology_Oncology_Primary_165_5] as reg
+on lung.PatientSID = reg.PatientSID
+order by patientsid -- 1931 rows
+
+select top 10 * from #ajz_icd_reg
+
+select distinct patientsid from #ajz_icd_reg
+where SitegpX like 'lung%'
+
+-- 96/100  ! Surprisingly most are in registry under LUNG.
+-- or 1176 / 1931, when we used 10,000 as our base param.
+-- 60.9%
+-- Bear in mind, this is anyone with ONE OR MORE visit for lung cancer, using OUTPATIENT VISITS ONLY.
+-- Not even the stringent criterion of TWO OR MORE visits with ICD tag for lung cancer.
