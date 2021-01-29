ae1eef76b05371040119216941ece5b7d4294d47
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-01-29 11:18:53 -0600

    Okay first plot

--- stage-vs-vamc.R
+++ stage-vs-vamc.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidyr)
 library(ggplot2)
 library(here)
 
@@ -6,7 +7,9 @@ colon = read.csv(here("ajz-colon-stg-sta3n.csv"), header=FALSE, col.names=c('sta
 lung  = read.csv(here("ajz-lung-stg-sta3n.csv"), header=FALSE, col.names=c('station', 'stage', 'count'))
 
 colon %>%
-mutate_at(vars(station, stage), ~ as.factor(.)) ->
+mutate_at(vars(station, stage), ~ as.factor(.)) %>%
+pivot_wider(names_from = stage, values_from = count) %>%
+mutate(early =`0` + I + II, late=III +IV, measure = late / (early+late))->
 colon_tidy
 
-head(colon_tidy)
+qplot(colon_tidy$measure) + labs(title='Distribution of early/late measure by VAMC', x='Measure', y='Count')
